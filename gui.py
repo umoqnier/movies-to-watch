f3739e112cd191bc33623613d179d51918f1cbc7
Diego Alberto Barriga Martínez <diegobarriga@protonmail.com>
2019-12-18 22:57:13 -0600

    refactor(Vista): Banner de pelicula
    	Cambio al banner de la pelicula en turno para ver. Corrección de
    	error en el despliegue de filas de tarjetas

--- gui.py
+++ gui.py
@@ -8,8 +8,11 @@ app = Flask(__name__)
 def index():
     current_movies = movie_selector(1)[0]
     movies = get_movies()
-    return render_template("index.html", current=current_movies, movies=movies)
+    total_movies = len(movies)
+    return render_template("index.html", current=current_movies, movies=movies,
+                           total_movies=total_movies,
+                           rows=int(total_movies / 4))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
